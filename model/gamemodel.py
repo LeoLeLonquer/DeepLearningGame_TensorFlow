3fc6da03e34271a11ef11c98ee899396dcc0989c
vchatela <valentindu64@hotmail.fr>
2016-05-07 20:38:11 +0200

    Fixed not working but wait for patch on situation

--- model/gamemodel.py
+++ model/gamemodel.py
@@ -55,7 +55,6 @@ class GameModel(Model):
 		self.situation = ssituation.Situation()
 		self.parser = pparser.Parser(self.situation)
 		self.communication = ccommunication.Communication(self.parser, server, server_fd)
-		assert(self.communication is not None)
 
 	def build_model(self):
 		# network weights
@@ -65,11 +64,11 @@ class GameModel(Model):
 		W_conv2 = weight_variable([3, 3, 16, 16])
 		b_conv2 = bias_variable([16])
 		
-		W_fc2 = weight_variable([512, ACTIONS])
+		W_fc2 = weight_variable([144, ACTIONS])
 		b_fc2 = bias_variable([ACTIONS])
 		
-		W_fc1 = weight_variable([512, 512])
-		b_fc1 = bias_variable([512])
+		W_fc1 = weight_variable([144, 144])
+		b_fc1 = bias_variable([144])
 		
 		# input layer
 		self.input_layer = tf.placeholder("float", [None, 10, 10, 4])
@@ -80,7 +79,7 @@ class GameModel(Model):
 		
 		h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 1) + b_conv2)
 		
-		h_conv3_flat = tf.reshape(h_conv2, [-1, 512])
+		h_conv3_flat = tf.reshape(h_conv2, [-1, 144])
 		
 		self.h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
 		
@@ -88,27 +87,26 @@ class GameModel(Model):
 		self.readout = tf.matmul(self.h_fc1, W_fc2) + b_fc2
 		
 	def play_random_action(self, piece_id):
-		situation = self.situation
-		piece = situation.player_pieces[piece_id]
-		loc = situation.get_player_piece_location(piece_id)
-		depth = situation.pieces_types[piece.piece_type].speed
+		piece = self.situation.player_pieces[piece_id]
+		loc = self.situation.get_player_piece_location(piece_id)
+		depth = self.situation.pieces_types[piece.piece_type].speed
 		def crossable(a):
 			qa, ra = a
-			return situation.is_in_map((qa, ra)) and \
-					situation.can_player_piece_be_on(piece_id, (qa, ra)) and \
-					situation.is_tile_none((qa, ra))
+			return self.situation.is_in_map((qa, ra)) and \
+					self.situation.can_player_piece_be_on(piece_id, (qa, ra)) and \
+					self.situation.is_tile_none((qa, ra))
 		def neighbors(a):
 			qa, ra = a
 			result = []
 			for direction in algos.directions:
 				qd, rd = direction
 				qb, rb = qa + qd, ra + rd
-				if situation.is_in_map((qb, rb)) and situation.can_player_piece_be_on(piece_id, (qb, rb)):
+				if self.situation.is_in_map((qb, rb)) and self.situation.can_player_piece_be_on(piece_id, (qb, rb)):
 					result.append( (qb, rb) )
 			return result
 		def cost(x, y):
 			return 1
-		heuristic = situation.get_tiles_distance
+		heuristic = self.situation.get_tiles_distance
 		destinations, came_from = algos.breadth_first_search_all(loc, depth, neighbors, cost, heuristic, crossable)
 		if len(destinations) > 0:
 			dest = random.choice(destinations)
@@ -121,7 +119,6 @@ class GameModel(Model):
 		  learning_rate: float, The learning rate [0.001]
 		  checkpoint_dir: str, The path for checkpoints to be saved [checkpoint]
 		"""
-		situation = self.situation
 		self.learning_rate = learning_rate
 		self.checkpoint_dir = checkpoint_dir
 
@@ -169,21 +166,43 @@ class GameModel(Model):
 		action_index = [None]*NB_CHUNK
 		r_t = [None]*NB_CHUNK
 		
+		self.situation.check()
+		print len(self.situation.player_cities)
+		for city_id in self.situation.player_cities:
+			think.choose_relevant_random_production(self.situation, self.communication, city_id)
 		while 1:
 			self.communication.wait()
 
-			debug("nb cities: %d" % len(situation.player_cities))
-			debug("nb pieces: %d" % len(situation.player_pieces))
+			debug("nb cities: %d" % len(self.situation.player_cities))
+			debug("nb pieces: %d" % len(self.situation.player_pieces))
 
-			situation.check()
+			self.situation.check()
 			
-			chunks = situation.split(10)
-			
-			for i in range(len(chunks)):
-				s_t[i] = chunks[i]
-				print s_t[i]
-				#print len(s_t[i])
+			chunks = self.situation.split(10)
 				
+			for i in range(len(chunks)):
+				chunk = chunks[i]
+				
+				for q in range(len(chunk)):
+					for r in range(len(chunk[q])):
+						if chunk[q][r].visible == False:
+							chunk[q][r] = 0
+						else:
+							if chunk[q][r].content == None:
+								if chunk[q][r].terrain == ssituation.GROUND:
+									chunk[q][r] = 1
+								else:
+									chunk[q][r] = 2
+							else:
+								if isinstance(chunk[q][r].content, ssituation.City):
+									chunk[q][r] = 3
+								elif isinstance(chunk[q][r].content, ssituation.OwnedCity):
+									chunk[q][r] = 4 + chunk[q][r].content.owner # 4 et 5 !!!
+								else:
+									chunk[q][r] = 6 + chunk[q][r].content.piece_type + chunk[q][r].content.owner * len(self.situation.pieces_types)
+				#print chunk
+				s_t[i] = chunk
+				s_t[i]  = np.stack((s_t[i] , s_t[i] , s_t[i] ,s_t[i] ), axis = 2) #check for last four
 				#evaluate with current model 
 				readout_t[i] = self.readout.eval(feed_dict = {self.input_layer : [s_t[i]]})[0]
 	
@@ -202,10 +221,11 @@ class GameModel(Model):
 		
 		
 			# play actions
-			piece_ids = situation.player_pieces.keys()
+			piece_ids = self.situation.player_pieces.keys()
+			print len(piece_ids)
 			for piece_id in piece_ids:
 				#while can go further 
-				for n in range(the_situation.pieces_types[piece.piece_type].speed):
+				for n in range(self.situation.pieces_types[piece.piece_type].speed):
 					## Get the output vector from the right chunk and multiply it to ProbaVector
 						# for ex: Water = 0 - City = 0.8 - T1 = 0.3 etc..
 				
@@ -214,14 +234,14 @@ class GameModel(Model):
 						#self.play_random_action(piece_id)			
 					
 						# OR go in the right direction
-					directions = the_situation.directions
-					if not the_situation.is_player_piece(piece_id):
+					directions = self.situation.directions
+					if not self.situation.is_player_piece(piece_id):
 						break
-					loc = the_situation.get_player_piece_location(piece_id)
+					loc = self.situation.get_player_piece_location(piece_id)
 					result = []
 					for dir in range(len(directions)):
 						next_location = loc[0] + directions[dir][0], loc[1] + directions[dir][1]
-						if the_situation.can_player_piece_be_on(piece_id, next_location):
+						if self.situation.can_player_piece_be_on(piece_id, next_location):
 							result.append(dir)
 					if len(result) > 0:
 						direction = random.choice(result) # choose the one gave by the output_vector x ProbaVector
@@ -229,14 +249,13 @@ class GameModel(Model):
 					
 				## Observe the action and evaluate the result (Q function)
 					# check only if alive				
-				if not the_situation.is_player_piece(piece_id):
+				if not self.situation.is_player_piece(piece_id):
 					r_t[i] = -1
 				else:
 					r_t[i] = 1
-				
-				self.communication.end_turn()
-				situation.check()
-				chunks = situation.split(10)
+
+				self.situation.check()
+				chunks = self.situation.split(10)
 				# TODO : check if game ended
 				terminal = 0
 				
@@ -271,7 +290,7 @@ class GameModel(Model):
 							y : y_batch,
 							a : a_batch,
 							self.input_layer : s_j_batch})
-
+			self.communication.end_turn()
 			# update the old values
 			for i in range(len(chunks)):
 				s_t[i] = s_t1[i]
