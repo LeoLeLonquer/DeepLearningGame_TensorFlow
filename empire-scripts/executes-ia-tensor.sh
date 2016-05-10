dcf07bda48036dc31525d7d3fbb0f354e5d641e9
vchatela <valentindu64@hotmail.fr>
2016-05-10 21:15:36 +0200

    Fix script

--- empire-scripts/executes-ia-tensor.sh
+++ empire-scripts/executes-ia-tensor.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/bash
 
 SPORT=$(( ( RANDOM % 1000 ) + 2000 ))
-PPORT1=$(( ( RANDOM % 1000 ) + 3000 ))
-PPORT2=$(( ( RANDOM % 1000 ) + 4000 ))
-OPORT1=$(( ( RANDOM % 1000 ) + 5000 ))
-OPORT2=$(( ( RANDOM % 1000 ) + 6000 ))
 
 IA1=$1
 
@@ -20,19 +16,21 @@ NBWIN0=0
 NBWIN1=0
 
 NB=2
+PART=0
 while test $NB -gt 0; do
+	echo "Partie $PART démarrée"
 	NB=`expr $NB - 1`
+	PART=`expr $PART +1`
 	# Demarrage des programmes.
-	launch_xterm "./empire-server/Main.native -sport ${SPORT} 2>&1 | tee out_S" SPID
+	./empire-server/Main.native -sport ${SPORT}  > out_S 2>&1 & 
+	SPID=$!
 	sleep 1
-	launch_xterm "./empire-tee/tee.py localhost ${SPORT} ${PPORT1} ${OPORT1}" TPID1
-	launch_xterm "./empire-client/Main.native -obs -sport ${OPORT1}" OPID1
-	launch_xterm "./empire-captain/ai${IA1}.py localhost ${PPORT1} 2>&1 | tee out_P1" PPID1
-	launch_xterm "./empire-tee/tee.py localhost ${SPORT} ${PPORT2} ${OPORT2}" TPID2
-	launch_xterm "./empire-client/Main.native -obs -sport ${OPORT2}" OPID2
-	launch_xterm "python ./DeepLearningGame_TensorFlow/main.py localhost ${PPORT2} > out_IA | tee out_P2" PPID2
+	python ./empire-captain/ai${IA1}.py localhost ${SPORT} > out_P1 2>&1 &
+	PPID1=$!
+	python ./DeepLearningGame_TensorFlow/main.py localhost ${SPORT} > out_P2 2>&1 &
+	PPID2=$!
 
-	PIDS="${SPID} ${TPID1} ${OPID1} ${PPID1} ${TPID2} ${OPID2} ${PPID2}"
+	PIDS="${SPID} ${PPID1} ${PPID2}"
 
 	# Regarde si un des programmes est stoppe
 	STOPPED=0
@@ -56,7 +54,6 @@ while test $NB -gt 0; do
 			exit
 		fi
 	fi
-	echo "END PART"
-	echo "END PART $NBWIN0 $NBWIN1"
+	echo "END PART $PART : $NBWIN0 $NBWIN1"
 done
 echo $NBWIN0 $NBWIN1
